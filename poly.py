d3105893b95acfa757ff6f9512edd93d1bbde08a
yashwanthraobachupally <ymbachupally@gmail.com>
2026-09-03 19:31:19 +0530

    Create poly.py

---
+++ poly.py
@@ -0,0 +1,13 @@
+class Dog
+  def speak(self):
+    return "Woof!"
+
+class Cat:
+  def speak (self):
+    return "Meow!"
+
+animals = [Dog(),Cat()]
+
+for animal in animals:
+  print(animal.speak())
+  
