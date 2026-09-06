3272db78378148727acc3fc88c4c0e4ebc90a7c1
human <ymbachupally@gmail.com>
2026-09-06 22:49:27 +0530

    Fix get_product function and add add_product route

--- app1.py
+++ app1.py
@@ -5,4 +5,11 @@ def home():
 
 @app.route("/product/<int:item_id>")
 def get_product(item_id):
-  return jsonify
+  return jsonify({"product_id": item_id, "status": "available"})
+
+
+@app.route("/add-product", methods=(['PosT'])
+def add_product():
+  if __name__ == "__main__":
+    app.run(debug=True)
+
